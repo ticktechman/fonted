070ea6cd33c0c2035a44a832a6abb6c0e01578b2
ticktechman <geek.wystan@gmail.com>
2025-03-28 13:13:12 +0800

    change icon

--- content.js
+++ content.js
@@ -9,5 +9,29 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
     `;
     document.head.appendChild(styleEl);
   }
+  document.querySelectorAll("table .weui-desktop-simple-appmsg__thumb").forEach(el => {
+      el.style.paddingBottom = "40px";
+      el.style.width = "80px";
+  });
+  document.querySelectorAll("table td").forEach(el => {
+    el.style.padding = "6px";
+  });
+  document.querySelectorAll(".weui-desktop-mass__content .weui-desktop-mass-appmsg__thumb").forEach(el => {
+    el.style.width = el.style.height = "40px";
+  });
+  document.querySelectorAll(".weui-desktop-mass__content .weui-desktop-mass__overview").forEach(el => {
+    el.style.minHeight = "0px";
+  });
+  document.querySelectorAll(".publish_content div.weui-desktop-block").forEach(el => {
+    el.style.marginBottom = "4px";
+  });
+  document.querySelectorAll(".weui-desktop-block .weui-desktop-block__main").forEach(el => {
+    el.style.paddingTop = el.style.paddingBottom = "4px";
+  });
+  document.querySelectorAll("li.weui-desktop-sub-menu__item a").forEach(el => {
+    if (el.href.includes("count=")) {
+        el.href = el.href.replace(/(count=)\d+/, "$1100");
+    }
+  });
 });
 
